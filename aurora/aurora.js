92366664c31fe0c66be2d8f5addded33f180665f
apbodnar <abodnar@mines.edu>
2013-11-25 09:46:21 -0800

    hurrr

--- aurora/aurora.js
+++ aurora/aurora.js
@@ -1,6 +1,6 @@
 var gl;
 var num_quads = 300;
-var num_strips = 5;
+var num_strips = 6;
 var crunch = 0.02;
 var offsets = [{}];
 var persp = mat4.create();
@@ -77,9 +77,6 @@ function initShaders() {
   gl.useProgram(shaderProgram);
   shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
   gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
-
-  shaderProgram.vertexColorAttribute = gl.getAttribLocation(shaderProgram, "aVertexTrans");
-  gl.enableVertexAttribArray(shaderProgram.vertexColorAttribute);
   
   shaderProgram.tickUniform = gl.getUniformLocation(shaderProgram, "tick");
   shaderProgram.crunchUniform = gl.getUniformLocation(shaderProgram, "crunch");
@@ -91,22 +88,6 @@ function initShaders() {
 
 }
 
-function handleTextureLoaded(image, texture) {
-  gl.bindTexture(gl.TEXTURE_2D, texture);
-  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_NEAREST);
-  gl.generateMipmap(gl.TEXTURE_2D);
-  gl.bindTexture(gl.TEXTURE_2D, null);
-}
-
-function initTexture() {
-  cubeTexture = gl.createTexture();
-  cubeImage = new Image();
-  cubeImage.onload = function() { handleTextureLoaded(cubeImage, cubeTexture); }
-  cubeImage.src = "flower.png";
-}
-
 function generateStrip(num_quads, crunch) {
   var osy = 0.0;
   var osz = (Math.random() -0.5)*2.0;
@@ -116,24 +97,25 @@ function generateStrip(num_quads, crunch) {
   for(var i=0; i< 2*num_quads+1; i+=2){
     vert1 = [i*crunch-(crunch*num_quads), ((i+1)%2) - 0.5, -osz];
     vert2 = [i*crunch-(crunch*num_quads), (i%2) -0.5   ,  -osz];
-    vertices = vertices.concat(vert1);
-    vertices = vertices.concat(vert2);
+    vertices = vertices.concat(vert1,vert2);
   }
+  console.log(vertices);
   return vertices;
 }
 
 function initBuffers() {
-  for(var i=0; i< num_strips; i++){
-    squareVertexPositionBuffer[i] = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer[i]);
+	var vertices =[];
+	for(var i=0; i< num_strips; i++){
+		squareVertexPositionBuffer[i] = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer[i]);
+		vertices = [];
+		vertices = generateStrip(num_quads, crunch);
 
-    vertices = generateStrip(num_quads, crunch);
-    
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-    squareVertexPositionBuffer[i].itemSize = 3;
-    squareVertexPositionBuffer[i].numItems = 2*num_quads+2;
-  }
-  console.log(offsets);
+		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+		squareVertexPositionBuffer[i].itemSize = 3;
+		squareVertexPositionBuffer[i].numItems = 2*num_quads+2;
+	}
+	console.log(squareVertexPositionBuffer.length);
 }
 
 var lastTime = 0;
@@ -152,6 +134,7 @@ function drawScene() {
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
   for(var i=0 ; i< num_strips; i++){
+  gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
     gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer[i]);
     gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, squareVertexPositionBuffer[i].itemSize, gl.FLOAT, false, 0, 0);
 
@@ -179,12 +162,10 @@ function webGLStart() {
   initGL(canvas);
   initShaders();
   initBuffers();
-  //initTexture();
 
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
   gl.disable(gl.DEPTH_TEST);
   gl.enable(gl.BLEND);
   gl.blendFunc(gl.SRC_COLOR, gl.ONE);
   tick();
-  //drawScene();
 }
